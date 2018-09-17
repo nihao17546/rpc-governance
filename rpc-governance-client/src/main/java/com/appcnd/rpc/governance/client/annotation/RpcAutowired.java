af0c309212a6fbf4240836a42818ce0932a1accc
nihao17546 <1754644071nh>
2018-09-17 17:29:36 +0800

    客户端注解

---
+++ rpc-governance-client/src/main/java/com/appcnd/rpc/governance/client/annotation/RpcAutowired.java
@@ -0,0 +1,16 @@
+package com.appcnd.rpc.governance.client.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Autowired
+@Lazy
+public @interface RpcAutowired {
+}
